fe3a4dd2d3f0c8d3a454f1fd0ca0d098f911d889
NicoC-Dev <nicolasalejocampos@outlook.es>
2024-09-21 18:03:16 -0300

    cambios al crud sub cat

--- src/main/java/com/programacion_avanzada/mega_store/DTOs/SubCategoriaDTO.java
+++ src/main/java/com/programacion_avanzada/mega_store/DTOs/SubCategoriaDTO.java
@@ -1,14 +1,17 @@
 package com.programacion_avanzada.mega_store.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SubCategoriaDto {
 
     @NotBlank
+    @Pattern(regexp = "^[^\\d]*$")
     private String nombre;
 
     @NotBlank
+    @Pattern(regexp = "^[^\\d]*$")
     private String descripcion;
 }
