9cd6afe4b6ac35bec941a9f5b6e112f0ace4790b
alexisgtierrz <alexisgutierrez1072@gmail.com>
2024-10-29 17:02:42 -0300

    Implementacion de Tests a MarcaDto con validaciones agregadas a dicha clase

--- src/main/java/com/programacion_avanzada/mega_store/DTOs/MarcaDto.java
+++ src/main/java/com/programacion_avanzada/mega_store/DTOs/MarcaDto.java
@@ -1,25 +1,40 @@
 package com.programacion_avanzada.mega_store.DTOs;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.Set;
+
 @Data
 public class MarcaDto {
 
     private long id;
 
-    @NotBlank
+    @NotBlank(message = "El nombre no debe estar vacío.")
     @Size(min = 2, max = 64)
-    @Pattern(regexp = "^[^\\d]*$")
+    @Pattern(regexp = "^[^\\d]*$", message = "El nombre no debe contener dígitos.")
+    @Pattern(regexp = "^[^\\s]*$", message = "El nombre no debe contener espacios en blanco.")
     private String nombre;
 
-    @NotBlank
-    @Size(min = 2, max = 64)
-    @Pattern(regexp = "^[^\\d]*$")
+    // Permitir nulo o vacío en la descripción
+    @Size(max = 100, message = "La descripción no debe exceder los 100 caracteres.")
+    @Pattern(regexp = "^[^\\d]*$", message = "La descripción no debe contener dígitos.")
     private String descripcion;
 
     private boolean estaActivo;
 
+    // Método que valida los campos de acuerdo con las anotaciones
+    public boolean esValido() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
 
+        Set<ConstraintViolation<MarcaDto>> violations = validator.validate(this);
+        return violations.isEmpty();
+    }
 }
