c65e2d0af166975ca623556275c0d6f5ee43416c
alexisgtierrz <alexisgutierrez1072@gmail.com>
2024-12-04 20:37:58 -0300

    Test de integracion

--- src/main/java/com/programacion_avanzada/mega_store/Repository/CategoriaRepository.java
+++ src/main/java/com/programacion_avanzada/mega_store/Repository/CategoriaRepository.java
@@ -1,6 +1,8 @@
 package com.programacion_avanzada.mega_store.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.programacion_avanzada.mega_store.Modelos.Categoria;
@@ -13,4 +15,6 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
     //Metodo para buscar categorias que esten solamente activos.
     List<Categoria> findAllByEstaActivoIsTrue();
+
+    Optional<Categoria> findByNombre(String nombre);
 }
